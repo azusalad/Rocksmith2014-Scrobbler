b8cd0ac1dbb6d798d01feda4f2c905f9d79541c0
azusalad <68360714+azusalad@users.noreply.github.com>
2024-12-20 19:19:33 -0600

    Created functions to get song data and to determine when to scrobble.

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 CURRENT_SONG_HTML = ""
+
+# ---------------------------------------
+# Probably don't want to change the values below
+# ---------------------------------------
+
+# Submit scrobble when this many seconds away from the end of the song
+END_THRESHOLD = 3
+# Interval time in seconds between checking song duration
+SLEEP_INTERVAL = 1
+# Interval time in seconds to wait after submitting a scrobble
+SCROBBLE_TIMEOUT = 15
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
 
 from config import *
 
@@ -12,5 +13,41 @@ class RocksmithScrobbler:
     self.artist = ""
     self.title = ""
     self.album = ""
-
     driver.get(CURRENT_SONG_HTML)
+    sleep(10)  # TODO: Replace this with a WebDriverWait
+
+  def run(self):
+    # TODO: Add scrobbler connection here
+    self.scrobble_loop()
+  
+  def scrobble_loop(self):
+    if self.end_of_song(driver.find_element(By.CLASS_NAME, "progress_bar_text").text):
+      self.artist = driver.find_element(By.CLASS_NAME, "artist_name").get_attribute("data-stroke")
+      self.title = driver.find_element(By.CLASS_NAME, "song_name").get_attribute("data-stroke")
+      self.album = driver.find_element(By.CLASS_NAME, "album_name").get_attribute("data-stroke")
+      self.scrobble()
+    sleep(SLEEP_INTERVAL)
+
+  def end_of_song(self, progress_text: str) -> bool:
+    current_time = progress.split("/")[0]
+    total_time = progress.split("/")[1]
+
+    current_seconds = int(current_time.split(":")[0]) * 60 + int(current_time.split(":")[1])
+    total_seconds = int(total_time.split(":")[0]) * 60 + int(total_time.split(":")[1])
+    return (total_seconds - current_seconds) <= END_THRESHOLD
+
+  def clear_data(self):
+    self.artist = ""
+    self.title = ""
+    self.album = ""
+
+  def scrobble(self):
+    # TODO: Scrobble to last.fm
+    self.clear_data()
+    sleep(SCROBBLE_TIMEOUT)
+    pass
+
+
+if __name__ == "__main__":
+  scrobbler = RocksmithScrobbler()
+  scrobbler.run()
